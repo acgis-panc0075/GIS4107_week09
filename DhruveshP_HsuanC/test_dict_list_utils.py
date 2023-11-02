9f58052a16e983951ab475563ff79ac69cd2a2fa
acgis-chen0971 <144171055+acgis-chen0971@users.noreply.github.com>
2023-11-02 09:11:38 -0400

    Hsuan

---
+++ DhruveshP_HsuanC/test_dict_list_utils.py
@@ -0,0 +1,43 @@
+#-------------------------------------------------------------------------------
+# Name:        test_dict_list_utils.py
+#
+# Purpose:     Test functions for functions in dict_list_utils.py
+#
+# Author:      David Viljoen
+#
+# Created:     20/10/2021
+# Last update: 31/10/2022
+#-------------------------------------------------------------------------------
+
+import dict_list_utils as dlu
+
+
+
+def test_get_missing_keys():
+    dict_ref = {1:1, 2:2, 3:3}
+    dict_to_compare = {2:2}
+    expected = [1, 3]
+    actual = dlu.get_missing_keys(dict_ref, dict_to_compare)
+    assert expected == actual
+
+
+def test_get_missing_keys_with_count():
+    dict_ref = {1:1, 2:2, 3:3}
+    dict_to_compare = {2:2}
+    expected = (2, [1, 3])
+    actual = dlu.get_missing_keys_with_count(dict_ref, dict_to_compare)
+    assert expected == actual
+
+
+def test_get_unique():
+    in_list = [1, 2, 2, 3]
+    expected = [1, 2, 3]
+    actual = dlu.get_unique(in_list)
+    assert expected == actual
+
+
+def test_flatten_list():
+    in_list = [1, (2,3), [4,5]]
+    expected = [1, 2, 3, 4, 5]
+    actual = dlu.flatten_list([in_list])
+    assert expected == actual
